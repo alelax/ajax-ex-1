d12901112ca0daec30a4a4c73e173bbc6e8432e3
alelax <alessandro.lausdei@gmail.com>
2018-04-17 08:41:30 +0200

    Ex - ok

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
+/*
+
+   Griglia 6x6, ad ogni click parte una
+   richiesta AJAX che prende un
+   numero random da 1 a 9.
+   Se è <= 5 il quadrato diventa giallo,
+   se è > di 5 il quadrato diventa verde.
+   Il numero ottenuto appare al centro
+   del quadrato
+
+*/
+
 $(document).ready(function(){
 
    //Ciclo che genera 64 celle
@@ -5,56 +17,51 @@ $(document).ready(function(){
       $('.grid-container').append( "<div id=" + i + " class=grid-item></div>");
    }
 
-   //Distribuisco all'inizio del gioco quali quadratini devono essere settati come bombe
-   var bombs = distributeBombs();
-   // console.log(bombs);
-   // console.log(typeof(bombs[0]));
-
    $('.grid-item').click(function(){
 
+      var itemClicked = $(this);
       var id = parseInt(this.id);
 
-      if ( bombs.includes(id) ) {
+      //Ad ogni click dell'item rimuove la classe, se presente, che inizia con bg-
+      itemClicked.removeClass(function (index, css) {
+	      return (css.match (/\bbg-\S+/g) || []).join(' '); // removes anything that starts with "page-"
+      });
 
-         if ( !($(this).hasClass('bg-red')) ) {
-            $(this).addClass('bg-red');
-            updateScores('bg-red');
-            //updateBombs();
-         }
+      // if ( itemClicked.hasClass('bg-green') ) {
+      //    itemClicked.removeClass('bg-green');
+      // } else if ( itemClicked.hasClass('bg-yellow') ) {
+      //    itemClicked.removeClass('bg-yellow');
+      // }
 
-      } else {
-         if ( !($(this).hasClass('bg-green')) ) {
-            $(this).addClass('bg-green');
-            updateScores('bg-green');
-            //updatePoints();
+      console.log("id: " + id );
+
+      // var num = randomNumber(0, 9);
+      $.ajax({
+         url: "https://www.boolean.careers/api/random/int",
+         method: "GET",
+         success: function(data, state) {
+
+            if (itemClicked.hasClass('error') ) {
+               itemClicked.removeClass('error');
+            }
+
+            var randomNumber = data.response;
+            console.log(randomNumber);
+
+            itemClicked.text(randomNumber);
+
+            if (randomNumber <= 5) {
+               itemClicked.addClass('bg-yellow');
+            } else {
+               itemClicked.addClass('bg-green');
+            }
+
+         },
+         error: function(){
+            itemClicked.addClass('error');
          }
-      }
+      });
 
    });
 
 });
-
-
-//Funzione che assegna casualmente 15 bombe ai quadrati della griglia. Restituisce
-//un array contenente gli indici dei quadrati ai quali sono state assegnate le bombe
-function distributeBombs() {
-   var bombs = new Array();
-   var i = 0;
-   do {
-      var whatIsAbomb = randomNumber(0, 63);
-      if ( !(bombs.includes(whatIsAbomb)) ) {
-         bombs.push(whatIsAbomb);
-         i++;
-      }
-   } while ( i < 15 )
-   console.log(bombs);
-   return bombs;
-}
-
-
-//Funzione che genera un numero casuale compreso tra l'estremo inferiore start
-//e l'estremo superiore end, inclusi.
-
-function randomNumber(start, end) {
-   return Math.floor(Math.random() * (end - start + 1)) + start;
-}
